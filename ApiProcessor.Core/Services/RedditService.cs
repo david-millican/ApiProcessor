e19e507c0f0941d1aa16e5e564cda9cdee1be7e6
David Millican <david.millican@gmail.com>
2023-09-13 14:29:27 -0500

    tracking top posts and users

--- ApiProcessor.Core/Services/RedditService.cs
+++ ApiProcessor.Core/Services/RedditService.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 //using static System.Net.WebRequestMethods;
@@ -10,21 +11,24 @@ namespace ApiProcessor.Core.Services
 {
     public class RedditService
     {
-        private string? token = null;
-        private string? lastPost = null;
-        private string user_agent = "sample-api-processor";
-        private JsonSerializerOptions jsonOptions = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true
-        };
-        private HttpClient http;
+        private string? _token = null;
+        private string? _lastPost = null;
+        private string _user_agent = "sample-api-processor";
+        private JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        private HttpClient _http;
         private readonly ILogger<RedditService> _logger;
+        //TODO - use ConcurrentDictionary if we find a need to make this thread safe.
+        private Dictionary<string, int> _topPosts = new();
+        private Dictionary<string, int> _topUsers = new();
 
         public RedditService(ILogger<RedditService> logger)
         {
             _logger = logger;
-            http = new HttpClient();
-            http.DefaultRequestHeaders.UserAgent.ParseAdd(user_agent);
+            _http = new HttpClient();
+            _http.DefaultRequestHeaders.UserAgent.ParseAdd(_user_agent);
         }
 
         public async Task ExecuteAsync()
@@ -49,43 +53,93 @@ namespace ApiProcessor.Core.Services
 
             message.Content = new StringContent($"grant_type=https://oauth.reddit.com/grants/installed_client&device_id={device_id}", Encoding.UTF8, "application/x-www-form-urlencoded");
 
-            var tokenResponse = await http.SendAsync(message);
+            var tokenResponse = await _http.SendAsync(message);
 
             var tokenContent = tokenResponse.Content.ReadAsStringAsync().Result;
 
-            token = JsonDocument.Parse(tokenContent).RootElement.GetProperty("access_token").GetString();
+            _token = JsonDocument.Parse(tokenContent).RootElement.GetProperty("access_token").GetString();
         }
         
         public async Task PollApi()
         {
             var baseUrl = "https://oauth.reddit.com/r/funny/new";
             var parameters = new Dictionary<string, string>() { { "limit", "100" } };
-            if (!String.IsNullOrEmpty(lastPost))
+            if (!String.IsNullOrEmpty(_lastPost))
             {
-                parameters.Add("after", lastPost);
+                parameters.Add("after", _lastPost);
             }
             var apiUrl = new Uri(QueryHelpers.AddQueryString(baseUrl, parameters));
 
             var apiMessage = new HttpRequestMessage(HttpMethod.Get, apiUrl);
             apiMessage.Headers.Authorization = new AuthenticationHeaderValue(
                 "Bearer",
-                token);
+                _token);
 
-            var apiResponse = http.SendAsync(apiMessage).Result;
+            var apiResponse = _http.SendAsync(apiMessage).Result;
 
             var apiContent = await apiResponse.Content.ReadAsStringAsync();
-            var subRedditRoot = JsonSerializer.Deserialize<RedditResponse>(apiContent, jsonOptions);
+            var subRedditRoot = JsonSerializer.Deserialize<RedditResponse>(apiContent, _jsonOptions);
             if (subRedditRoot != null)
             {
                 var posts = subRedditRoot.Data?.Children;
 
-                if (posts != null && posts.Any())
+                if (posts != null)
                 {
-                    //TODO - ILogger
-                    _logger.LogDebug(posts.First().Data?.Author);
-                    _logger.LogDebug(posts.First().Data?.UpVotes.ToString());
+                    foreach (var post in posts)
+                    {
+                        if (post != null)
+                        {
+                            if (post.Data == null)
+                            {
+                                _logger.LogError($"empty post data found {post}");
+                                continue;
+                            }
+                            if (String.IsNullOrEmpty(post.Data.Id))
+                            {
+                                _logger.LogError($"id missing from post {post}");
+                                continue;
+                            }
+                            if (post.Data.UpVotes == null)
+                            {
+                                _logger.LogError($"upvotes missing from post {post.Data.Id}");
+                                continue;
+                            }
+                            if (!_topPosts.TryAdd(post.Data.Id.ToString(), (int)post.Data.UpVotes))
+                            {
+                                _logger.LogError($"failed to add post {post.Data.Id} to topPosts");
+                            }
+                            if (post.Data.Author == null)
+                            {
+                                _logger.LogError($"author missing from post {post.Data.Id}");
+                                continue;
+                            }
+                            if (_topUsers.ContainsKey(post.Data.Author.ToString()))
+                            {
+                                _topUsers[post.Data.Author.ToString()]++;
+                            }
+                            else
+                            {
+                                if (!_topUsers.TryAdd(post.Data.Author.ToString(), 1))
+                                {
+                                    _logger.LogError($"failed to add post {post.Data.Id} to topUsers");
+                                }
+                            }
+                        }
+                    }
+                } else
+                {
+
                 }
             }
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> GetTopPosts(int count) {
+            return _topPosts.OrderByDescending(x => x.Value).Take(count);
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> GetTopUsers(int count)
+        {
+            return _topUsers.OrderByDescending(x => x.Value).Take(count);
         }
     }
 }
--- ApiProcessor.Test/Services/RedditServiceTest.cs
+++ ApiProcessor.Test/Services/RedditServiceTest.cs
@@ -1,22 +1,37 @@
-﻿namespace ApiProcessor.Test.Services
+﻿using ApiProcessor.Core.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ApiProcessor.Test.Services
 {
     public class RedditServiceTest
     {
+        public readonly Mock<ILogger<RedditService>> _mockLoger = new();
+
         [Fact]
         public void ServiceReturnsPostsWithMostUpvotes()
         {
-            // Mock processing a list of posts with varying number of upvotes
-            // Request top posts
-            // Assert correct result
-            Assert.True(false);
+            var reddit = new RedditService(_mockLoger.Object);
+
+            reddit.ExecuteAsync().Wait();
+            var requestedCount = 3;
+            var topPosts = reddit.GetTopPosts(requestedCount);
+
+            Assert.True(topPosts.Count() <= requestedCount);
+            //TODO - mock response and assert proper top posts
         }
 
         [Fact]
         public void ServiceReturnsUsersWithMostPosts()
         {
-            // Mock processing a list of posts having some repeat users
-            // Request top users
-            // Assert correct result 
+            var reddit = new RedditService(_mockLoger.Object);
+            
+            reddit.ExecuteAsync().Wait();
+            var requestedCount = 3;
+            var topUsers = reddit.GetTopUsers(requestedCount);
+
+            Assert.True(topUsers.Count() <= requestedCount);
+            //TODO - mock response and assert proper top users
         }
     }
 }
